05a2b976f536c021410341e3f6b5a0d03e284d68
ababup1192 <ababup1192@gmail.com>
2016-02-11 15:13:36 +0900

    Divide tests

--- src/main/java/org/ababup1192/Book.java
+++ src/main/java/org/ababup1192/Book.java
@@ -1,33 +1,46 @@
 package org.ababup1192;
 
 import com.iciql.Iciql;
+import lombok.experimental.Accessors;
 
 import java.io.Serializable;
 import java.util.Objects;
 
-@Iciql.IQTable(name="book")
+@Accessors(fluent = true)
+@Iciql.IQTable(name = "book", primaryKey = "id")
 public class Book implements Serializable {
-
     private static final long serialVersionUID = 1L;
 
-    @Iciql.IQColumn(primaryKey=true, autoIncrement = true, nullable = false)
-    public Integer id;
+    @Iciql.IQColumn(primaryKey = true, autoIncrement = true)
+    public Long id;
 
-    @Iciql.IQColumn(length=254, nullable=false)
+    @Iciql.IQColumn(length = 254, nullable = false)
     public String title;
 
-    public Book() {}
+    public Book() {
+    }
+
+    public Book(String title) {
+        this.id = null;
+        this.title = title;
+    }
+
+
+    public Book(Long id, String title) {
+        this.id = id;
+        this.title = title;
+    }
 
     @Override
-    public String toString(){
+    public String toString() {
         return "Book(" + id + ", " + title + ")";
     }
 
-    public boolean equals(Object target){
-        if(this == target) return true;
-        else if(!(target instanceof Book)){
+    public boolean equals(Object target) {
+        if (this == target) return true;
+        else if (!(target instanceof Book)) {
             return false;
-        }else{
+        } else {
             Book targetBook = (Book) target;
             return Objects.equals(this.id, targetBook.id) &&
                     Objects.equals(this.title, targetBook.title);
--- src/test/java/org/ababup1192/BookTest.java
+++ src/test/java/org/ababup1192/BookTest.java
@@ -1,58 +1,96 @@
 package org.ababup1192;
 
 import com.iciql.Db;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.assertThat;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
 
 public class BookTest {
-    Db db;
+    private Db db;
+    private List<Book> insertedBooks;
+    private List<Book> initialABooks;
+    private static final int NUM_OF_BOOKS = 1000;
 
     /*
         CREATE TABLE book (
-            id       int8    primary key,
-            title    varchar(254)
+            id       SERIAL PRIMARY KEY,
+            title    VARCHAR(254)
         );
      */
 
     @Before
     public void setDb() {
+        // 初期化
         db = Db.open("jdbc:postgresql://localhost:5432/vagrant", "vagrant", "vagrant");
+        db.dropTable(Book.class);
+
+        long bookCount = 0;
+        insertedBooks = new ArrayList<>();
+        initialABooks = new ArrayList<>();
+
+        for (int i = 0; i < NUM_OF_BOOKS; i++) {
+            String title = RandomStringUtils.randomAlphabetic(100).toLowerCase();
+            Book book = new Book(title);
+
+            // 生成した本をDBに格納。
+            db.insert(book);
+            book.id = ++bookCount;
+
+            // イニシャルが「a」だったら
+            if (title.matches("a\\w.+")) {
+                initialABooks.add(book);
+            }
+            insertedBooks.add(book);
+        }
     }
 
     @Test
-    public void testCRUD() throws Exception {
-        Book book = new Book();
-        // テーブルを削除
-        db.dropTable(Book.class);
+    public void testSelect() throws Exception {
+        // Bookテーブルから選択
+        List<Book> selectedBooks = db.from(new Book()).select();
 
-        // テーブルが空であることを確認。
-        List<Book> emptyBooks = db.from(book).select();
-        assertThat(emptyBooks.size(), is(0));
-
-        Book newBook = new Book();
-        newBook.title = "title";
-
-        // 新しい本を挿入し、タイトルと一致するかを確認
-        db.insert(newBook);
-        Book findBook = db.from(book).where(book.title).is("title").selectFirst();
-        assertThat(newBook.title, is(findBook.title));
-
-        // レコードを更新して、新しいタイトルと一致するか確認。
-        findBook.title = "updateTitle";
-        db.update(findBook);
-        Book updatedBook = db.from(book).where(book.title).is("updateTitle").selectFirst();
-
-        assertThat(findBook, is(updatedBook));
-
-        db.delete(updatedBook);
-
-        // 再びテーブルが空であることを確認。
-        emptyBooks = db.from(book).select();
-        assertThat(emptyBooks.size(), is(0));
+        // 挿入した本と選択した本のリストが一致するかどうか。
+        assertThat(selectedBooks, is(insertedBooks));
     }
+
+    @Test
+    public void testSelectWithFilter() throws Exception {
+        // Bookテーブルからイニシャルが「a」であるリストを選択
+        Book book = new Book();
+
+        List<Book> selectedBooks = db.from(book).where(book.title).like("a%").select();
+
+        // イニシャルが「a」である本のリストと選択した本のリストが一致するかどうか。
+        assertThat(selectedBooks, is(initialABooks));
+    }
+
+    @Test
+    public void testInsert() throws Exception {
+        // Bookテーブルからイニシャルが「a」であるリストを選択
+        Book book = new Book();
+
+        // 新しく本をDBに格納
+        Book newBook = new Book("abc");
+        db.insert(newBook);
+
+        // Bookテーブルから選択
+        List<Book> selectedBooks = db.from(book).select();
+
+        // 挿入本リストの最後にも新しい本を挿入
+        newBook.id = new Integer(insertedBooks.size() + 1).longValue();
+        insertedBooks.add(newBook);
+
+        // 挿入したである本のリストと選択した本のリストが一致するかどうか。
+        assertThat(selectedBooks, is(insertedBooks));
+        // Bookテーブルの最後の本は挿入した本と一緒するか。
+        assertThat(selectedBooks.get(selectedBooks.size() - 1), is(newBook));
+    }
+
+
 }
